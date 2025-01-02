9a163eb6101c58ecde74c7f53f945eed9f403043
aroy23 <arnavroy@ucla.edu>
2025-01-02 18:33:46 -0500

    Uploading model

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,8 +4,7 @@ import App from "./App.jsx";
 import axios from "axios";
 import "./styles/index.css";
 
-axios.defaults.baseURL =
-  import.meta.env.VITE_API_URL || "http://localhost:8080";
+axios.defaults.baseURL = import.meta.env.VITE_API_URL;
 axios.defaults.headers.common["Content-Type"] = "application/json";
 
 axios.interceptors.request.use(function (config) {
